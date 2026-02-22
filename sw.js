2fe82c0ea6647ed525dde9159b5f13eb9b079351
houda789907-Learn <houda789907@gmail.com>
2026-02-22 14:55:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,36 @@
-const CACHE_NAME = 'adhkar-v1';
-const assets = ['./', './index.html', './manifest.json', './icon.png'];
+const CACHE_NAME = 'adhkar-v2'; // غيرنا الاسم لـ v2 عشان المتصفح يحس بالتغيير ويخزن من جديد
+
+// هنا بنضيف كل الملفات اللي عاوزينها تشتغل "Offline"
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  './quran.html',
+  './morning.html',
+  './evening.html',
+  './general-adhkar.html',
+  './tasks.html',
+  './tasbih.html',
+  './quran-duas.html',
+  './prophet-duas.html',
+  './istighfar.html',
+  './ramadan.html',
+  './istikhara.html',
+  './worry.html'
+];
 
 // تثبيت التطبيق وتخزين الملفات
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('جاري حفظ صفحات الأذكار للعمل بدون نت...');
+      return cache.addAll(assets);
+    })
   );
 });
 
-// تفعيل السيرفس وركر
+// تفعيل السيرفس وركر وتنظيف الكاش القديم
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
@@ -17,9 +39,11 @@ self.addEventListener('activate', event => {
   );
 });
 
-// تشغيل التطبيق حتى بدون إنترنت
+// تشغيل التطبيق وجلب الملفات من المخزن (Cache) عند انقطاع النت
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
   );
 });
